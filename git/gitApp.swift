79cf3babe79997f5aa447ea3df0d31081089bebb
nadiaaab <nadiabeauu@gmail.com>
2021-09-17 17:41:53 -0400

    Initial Commit

---
+++ git/gitApp.swift
@@ -0,0 +1,17 @@
+//
+//  gitApp.swift
+//  git
+//
+//  Created by Nadia Beauregard on 2021-09-17.
+//
+
+import SwiftUI
+
+@main
+struct gitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
